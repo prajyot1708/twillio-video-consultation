04077a6668bb0ddf90bc71e6f1d0d47659b9e7d9
Prajyot Ahire <AL4005@accionlabs.in>
2022-04-08 13:59:17 +0530

    updated code

--- src/shared/enum/roles.enum.ts
+++ src/shared/enum/roles.enum.ts
@@ -1,7 +1,5 @@
 export enum Role {
     PATIENT = 'patient',
     PROVIDER = 'provider',
-    TM2U_ADMIN = 'tm2u_admin',
-    CLINIC_CORDINATION = 'clinic_coordination',
-    TM2U_SUPER_ADMIN = 'tm2u_super_admin',
+    TM2U_ADMIN = 'tm2u_admin'
 }
